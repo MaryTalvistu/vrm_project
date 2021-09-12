c878c89d4f1ca963730e3c291d2c945560041ccf
Mary Talvistu <mary-ann.talvistu@khk.ee>
2021-09-12 14:14:23 +0300

    Created search with POST method

--- src/Controller/AlbumsController.php
+++ src/Controller/AlbumsController.php
@@ -12,7 +12,7 @@ class AlbumsController extends Controller
 	 	return $this->render($response, 'default.html', ['albums' => $albums]);
 	 }
 
-	 	 public function search(Request $request, Response $response)
+	 public function search(Request $request, Response $response)
 	 {	 	
 	 	$albums = json_decode(file_get_contents(__DIR__.'/../../data/albums.json'), true);
 
@@ -26,6 +26,22 @@ class AlbumsController extends Controller
 			}
 
 	 	return $this->render($response , 'search.html', ['query' => $query, 'albums' => $albums]);
+	 }
+
+	  public function form(Request $request, Response $response)
+	 {	 	
+	 	$albums = json_decode(file_get_contents(__DIR__.'/../../data/albums.json'), true);
+
+		$query = $request->getParam('q');
+
+		if($request->isPost()){
+			$albums = array_values(array_filter($albums, function($album) use ($query){
+				return strpos($album['title'], $query) !== false or strpos($album['artist'], $query) !== false;
+			}));
+
+			}
+
+	 	return $this->render($response , 'form.html', ['query' => $query, 'albums' => $albums]);
 	 }
 }
 
